5bee581796e3bf75697424c5391e89537605b714
EMMA3PO <58348618+EMMA3PO@users.noreply.github.com>
2020-01-09 11:30:49 -0600

    Add files via upload

---
+++ Tests/TestQuestion15.java
@@ -0,0 +1,18 @@
+//Emma Ko
+//12.12.19
+package TestUse;
+
+public class TestQuestion15 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int i = 0; {
+
+	while (i < 5) {
+		System.out.println ("11111");
+		i++;
+		}
+
+	}
+
+	}
+}
