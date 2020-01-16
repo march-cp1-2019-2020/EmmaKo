ed74ee5400d2b23db7f4ff155671b84b507d41d5
EMMA3PO <58348618+EMMA3PO@users.noreply.github.com>
2020-01-16 15:51:04 -0600

    Add files via upload
    This program is kind of finicky. If you type anything other than numbers into the console (like spaces) it won't work. Just type number and enter nothing else (until it asks for y/n).

---
+++ Exercises/Main.java
@@ -0,0 +1,39 @@
+//Emma Ko
+//1.16.20
+//Instrutions: This app only works if you enter the values without first putting spaces. Just Type.
+package ch03_ex2_AreaAndPerimeter;
+
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println ("Hi there! What perameters would you like to calculate?");
+		System.out.println();
+
+		 Scanner sc = new Scanner(System.in);        
+	        String choice = "y";
+	        while (choice.equalsIgnoreCase("y")) {
+	        	System.out.println ("Enter Length");
+	        	int length = Integer.parseInt(sc.nextLine());
+	        	
+	        	System.out.println ("Enter Width");
+	        	int width = Integer.parseInt(sc.nextLine());
+	        	
+	        	double Area = length * width;
+	        	double Perimeter = (length + width) * 2;
+	        	
+	        	System.out.println ("Area:            " + 
+	        			NumberFormat.getInstance().format (Area));
+	        	System.out.println ("Perimeter:       " + 
+	        			NumberFormat.getInstance().format(Perimeter));
+	        	
+	        	System.out.print("Do Another? (y/n): ");
+	            choice = sc.nextLine();
+	            System.out.println();
+	        }
+	        sc.close();
+	        System.out.println("Thank You For Using This App!");
+
+	}
+}
