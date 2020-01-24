dda756e65abc8cb3cf5207dfdd86af4e755b959a
EMMA3PO <58348618+EMMA3PO@users.noreply.github.com>
2020-01-23 20:48:28 -0600

    Add files via upload

---
+++ Cashier.java
@@ -0,0 +1,19 @@
+//Emma Ko
+//1.23.20
+public class Cashier {
+
+	public class Cashier extends Employee {
+	
+}
+	public double getRegisterNum (int num) {
+		return num;
+		
+	}
+	public double setRegisterNum(int num) {
+		return num;
+	}
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ Manager.java
@@ -0,0 +1,14 @@
+//Emma Ko
+//1.23.2020
+public class Manager {
+	public class Manager extends Employee {
+		
+	}
+	public void manageEmployees() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
