a20ad8bd97267c500d1e019829e24afb7d31a888
EMMA3PO <58348618+EMMA3PO@users.noreply.github.com>
2020-01-23 20:49:43 -0600

    Add files via upload
    Hi Ms. Norton,
    I accidentally uploaded to the wrong folder, here they are again.

---
+++ Exercises/Cook.java
@@ -0,0 +1,19 @@
+//Emma Ko
+//1.23.2020
+public class Cook {
+
+	Cook extends Employee {
+		
+	}
+	public void getCulinarySchool() {
+		
+	}
+	public void setCulinarySchool () {
+	return CulinarySchool;
+	
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Exercises/Employee.java
@@ -0,0 +1,32 @@
+//Emma Ko
+//1.23.2020
+public class Employee {
+
+	public void name (String Name) {
+		System.out.println ("Hi! My name is" + Name);
+	}
+	
+	public void snn () {
+		
+	}
+	
+	
+	public void dateOfBirth (String dateOfBirth) {
+		System.out.println ("I was born" + dateOfBirth);
+	}
+	public double paySalary (int hours) {
+		return hours * 40.00;
+	}
+	
+	
+	public void takeVacation() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
+	
+
