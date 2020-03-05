a1613ff636f6fbca6f9b611feec912ebd68ddbb0
EMMA3PO <58348618+EMMA3PO@users.noreply.github.com>
2020-03-05 12:55:32 -0600

    Add files via upload

---
+++ Exercises/ch15 ex1 Invoice/ch15_ex1_Invoice/src/murach/business/Invoice.java
@@ -0,0 +1,74 @@
+//Emma Ko
+//3.5.2020
+package murach.business;
+
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+public class Invoice {
+
+	// the instance variables
+	private ArrayList<LineItem> lineItems;
+	private LocalDateTime invoiceDate;
+
+	// the constructor
+	public Invoice() {
+		lineItems = new ArrayList<>();
+		invoiceDate = LocalDateTime.now();
+	}
+
+	public void addItem(LineItem lineItem) {
+		lineItems.add(lineItem);
+	}
+
+	public ArrayList<LineItem> getLineItems() {
+		return lineItems;
+	}
+
+	public double getTotal() {
+		double invoiceTotal = 0;
+		for (LineItem lineItem : lineItems) {
+			invoiceTotal += lineItem.getTotal();
+		}
+		return invoiceTotal;
+	}
+
+	public String getTotalFormatted() {
+		NumberFormat currency = NumberFormat.getCurrencyInstance();
+		return currency.format(getTotal());
+	}
+
+	public void setInvoiceDate(LocalDateTime invoiceDate) {
+		this.invoiceDate = invoiceDate;
+	}
+
+	public LocalDateTime getInvoiceDate() {
+		return invoiceDate;
+	}
+
+	public String getInvoiceDateFormatted() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM);
+		return dtf.format(invoiceDate);
+	}
+
+	public String getDueDate() {
+    
+		String DueDate;
+		return DueDate LocalDateTime newDateTime = invoiceDate.plus(30, ChronoUnit.DAYS);
+		//This error wants a ; at the end of the return statement but there is one. I don't know what to do. 
+    }
+
+	public String DueDateFormatted () {
+    	 DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDate(
+                FormatStyle.MEDIUM);        
+        return dtf.format(DueDate);
+    	
+    }
+	
+    
+    
+}
---
+++ Exercises/ch15 ex2 AgeCalculator/ch15_ex2_AgeCalculator/src/murach/agecalculator/AgeCalculatorApp.java
@@ -0,0 +1,41 @@
+//Emma Ko 3.5.2020
+//I can't figure out how to minus the date of birth from the current time. If you take that out everything else works fine. 
+package murach.agecalculator;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class AgeCalculatorApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Age Calculator\n");
+
+        // Get input from the the user
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your date of birth (YYYY-MM-DD): ");
+        String dateOfBirthString = sc.nextLine();
+        System.out.println();
+        
+        // Get and print user's date of birth
+        System.out.println("Your date of birth is " + dateOfBirthString);
+        
+        LocalDate currentDateString = LocalDate.now();
+        
+        // Get and print the current date
+        System.out.println("The current date is " + currentDateString); 
+
+        //Calculate the user's age
+        String Date1 = dateOfBirthString;
+        String Date2 = currentDateString;
+
+        Date date1 = format.parse(Date1);
+        Date date2 = format.parse(Date2);
+        long difference = date2.getDate() - date1.getDate();
+        
+        age = long difference;
+       
+        //Print the user's age
+        System.out.println("Your age is "+ age);
+        sc.close();
+    }
+}
