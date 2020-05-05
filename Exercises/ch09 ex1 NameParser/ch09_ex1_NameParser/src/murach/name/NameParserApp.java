3611d8a4c8aaa70af1df22b2bf3295d47ef6d910
EMMA3PO <58348618+EMMA3PO@users.noreply.github.com>
2020-05-05 16:33:58 -0500

    Add files via upload
    When I tried to do this exercise java wouldn't let me open some things. It said it was not a class path

---
+++ Exercises/ch09 ex1 NameParser/ch09_ex1_NameParser/src/murach/name/NameParserApp.java
@@ -0,0 +1,19 @@
+//Emma Ko 4.9.20
+//Java wouldn't let me run this for some reason, so I couldn't test it. It just keeps running the previous code I did.
+package murach.name;
+
+import java.util.Scanner;
+
+public class NameParserApp {
+
+    public static void Main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a name: ");
+        String name = sc.nextLine();
+        System.out.println();
+        sc.close();
+    String Name = name + "";
+    String Name += "";
+    }
+}
---
+++ Exercises/ch09 ex2 FutureValue/ch09_ex2_FutureValue/src/murach/ui/Console.java
@@ -0,0 +1,63 @@
+package murach.ui;
+
+import java.util.Scanner;
+
+public class Console {
+
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void displayLine() {
+		System.out.println();
+	}
+
+	public static void displayLine(String s) {
+		System.out.println(s);
+	}
+
+	public static String getString(String prompt) {
+		System.out.print(prompt);
+		String s = sc.nextLine();
+		return s;
+	}
+
+	public static int getInt(String prompt) {
+		int i = 0;
+		while (true) {
+			System.out.print(prompt);
+			try {
+				i = Integer.parseInt(sc.nextLine());
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("Error! Invalid integer. Try again.");
+			}
+		}
+		return i;
+	}
+
+	public static double getDouble(String prompt) {
+		double d = 0;
+		while (true) {
+			System.out.print(prompt);
+
+			if (d.charAt(i) == '$') {
+				d.deleteCharAt(i);
+				i--;
+			}
+
+			// I tried to copy the book but java won't let me use i, what should I put in
+			// instead?
+
+			if (d.charAt(i) == '%') {
+				d.deleteCharAt(i);
+				i--;
+			}
+			try {
+				d = Double.parseDouble(sc.nextLine());
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("Error! Invalid decimal. Try again.");
+			}
+		}
+		return d;
+	}
+}
