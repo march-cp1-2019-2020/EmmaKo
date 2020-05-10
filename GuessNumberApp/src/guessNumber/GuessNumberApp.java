5e543f23030c48a10ee02049ae5d3dead16251b3
nortoha <39999692+nortoha@users.noreply.github.com>
2020-05-10 16:36:11 -0500

    TEACHER NOTES
    Adding an implementation for your inRange method and changed the way to call it.  I could see this method being useful in many assignments.

--- GuessNumberApp/src/guessNumber/GuessNumberApp.java
+++ GuessNumberApp/src/guessNumber/GuessNumberApp.java
@@ -20,18 +20,23 @@ public class GuessNumberApp {
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 
-	int count = 1;
+		int count = 1;
 		{
-//change to while loop
+			// change to while loop
 			int guess = sc.nextInt();
 			System.out.println("You guessed: " + guess);
 
 			// TODO: compare the guess to the random number
 			System.out.println(Integer.compare((int) d, guess));
 
-		
 			// TODO: what should happen if the user enters a number outside the range?
+			//TEACHER NOTES: the way this is called here only checks if guess is greater than 0
+			// because your version of inRange returns 0 every time.  Look at my inRange method
+			// at the bottom of your code and how I call it here on line 39-41
 			if (guess > inRange(0, 10)) {
+				System.out.println("Out of Range!");
+			}
+			if(!inRange(guess, 0, 10, true)) {
 				System.out.println("Out of Range!");
 			}
 
@@ -47,8 +52,8 @@ public class GuessNumberApp {
 			if (guess == d) {
 				System.out.println("YES! YOU GUESSED IT");
 			}
-			
-		System.out.println ("You used " + count + " tries");
+
+			System.out.println("You used " + count + " tries");
 
 		}
 	}
@@ -58,4 +63,20 @@ public class GuessNumberApp {
 		return 0;
 	}
 
+	/*
+	 * TEACHER NOTES: here is my interpretation of a possible inRange method you could do
+	 */
+	public static boolean inRange(int num, int low, int high, boolean inclusive){
+	    if(inclusive){
+	        // include high and low as inside the range
+	        if((low <= num ) && (num <= high))
+	            return true;
+	    } else {
+	        // do not include high and low in the range. use strictly less than and greater than
+	    	if((low < num ) && (num < high))
+	            return true;
+	    }
+	    // the above conditions were not met so the result is false.   
+	    return false;
+	}
 }
