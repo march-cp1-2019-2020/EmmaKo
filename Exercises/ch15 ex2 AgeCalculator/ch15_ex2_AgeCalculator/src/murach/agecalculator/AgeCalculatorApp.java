c0bbd8db2bea2baa014e33c92d25d830570c3fe5
nortoha <39999692+nortoha@users.noreply.github.com>
2020-05-10 17:24:45 -0500

    TEACHER NOTES

--- Exercises/ch15 ex2 AgeCalculator/ch15_ex2_AgeCalculator/src/murach/agecalculator/AgeCalculatorApp.java
+++ Exercises/ch15 ex2 AgeCalculator/ch15_ex2_AgeCalculator/src/murach/agecalculator/AgeCalculatorApp.java
@@ -19,15 +19,19 @@ public class AgeCalculatorApp {
         // Get and print user's date of birth
         System.out.println("Your date of birth is " + dateOfBirthString);
         
+        //TEACHER NOTES:I wouldn't call this currentDateString because its not a String type.  Call it currentDate
         LocalDate currentDateString = LocalDate.now();
         
         // Get and print the current date
         System.out.println("The current date is " + currentDateString); 
 
         //Calculate the user's age
+        //TEACHER NOTES: variables should start with a lowercase letter
+        // why do you turn these to strings just to turn them back to Dates?
         String Date1 = dateOfBirthString;
         String Date2 = currentDateString;
 
+        //TEACHER NOTES: where does format come from?
         Date date1 = format.parse(Date1);
         Date date2 = format.parse(Date2);
         long difference = date2.getDate() - date1.getDate();
